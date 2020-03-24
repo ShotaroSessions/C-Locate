b3715f31f3d6a6f078c369b1e459a423e491ed7e
Shotaro Sessions <sessionsshotaro@gmail.com>
2020-03-24 15:20:58 -0400

    Reformatted, now counts results

--- locate.c
+++ locate.c
@@ -1,69 +1,41 @@
-#include <dirent.h>
+/* 
+    File: locate.c
+    Author: Shotaro Sessions
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define STACKMAX 100
+#include "locate.h"
 
-char *stack[STACKMAX];
-int top = -1;
-
-void push(char* str)
-{
-    if(top > STACKMAX - 1) {
-        perror("Stack overflow");
-        exit(0);
-    }
-    stack[++top] = str;
-}
-
-char *pop(void)
-{
-    if(top < 0) {
-        perror("Stack underflow");
-        exit(0);
-    }
-    return stack[top--];
-}
-
-char * concat(char*str1, char*str2)
-{
-    size_t len1 = strlen(str1), len2 = strlen(str2);
-    //char *concat_str = (char*) malloc(len1 + len2 + 1);
-    static char concat_str[1000];
-    memcpy(concat, str1, len1);
-    memcpy(concat+len1, str2, len2+1);
-    return concat_str;
-}
-
-char *get_path(void)
-{
-    static char *path = "";
-    path = concat(path, stack[0]);
-    for(int i = 1; i <= top; i++) {
-        path = concat(path, "/");
-        path = concat(path, stack[i]);
-    }
-    return path;
-}
-
-DIR *open_dir(char *path)
-{
-    DIR *folder = opendir(path);
-    if(folder == NULL)
-    {
-        perror("Unable to read directory");
-        exit(0);
-    }
-    return folder;
-}
-
-void recsearch(char*key, char*root)
+int locate(char*key, char*root)
 {
     char name[260];
     DIR *directory;
     struct dirent *entry;
+    int matches = 0, inner_matches;
+
+
+    /* TODO: add format string method */
+    for(int i = 0; i < strlen(root); i++) {
+        if(root[i] == '\n') {
+            root[i] = '\0';
+        }
+    }
+    for(int i = 0; i < strlen(key); i++) {
+        if(key[i] == '\n') {
+            key[i] = '\0';
+        }
+    }
+    /* FIXME: delete above for loops once format string method added*/
+
+
     directory = open_dir(root);
+    if(directory == NULL) {
+        return -1;
+    }
+
     while( (entry = readdir(directory)) ) {
         if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..")) {
             name[0] = '\0';
@@ -72,26 +44,28 @@ void recsearch(char*key, char*root)
             strcat(name, entry->d_name);
             if(strstr(name, key) != NULL) {
                 printf("\n%s", name);
-                //strcat(str, name);
-                //strcat(str, "\n");
+                matches++;
             }
             if(entry->d_type == DT_DIR) {
-                recsearch(key, name);
+                inner_matches = locate(key, name);
+                if(inner_matches == -1) {
+                    return -1;
+                }
+                matches += inner_matches;
             }
         }
     }
     closedir(directory);
+    return matches;
 }
 
-void locate(char*key, char*root)
+DIR *open_dir(char *path)
 {
-    //static char result[100000] = "";
-    recsearch(key, root);
-    //return result;
-}
-
-int main(void)
-{
-    
-    locate("priority_planner","/home/shotaro");
+    DIR *folder = opendir(path);
+    if(folder == NULL)
+    {
+        perror("Unable to read directory");
+        return NULL;
+    }
+    return folder;
 }
